bdc739a5c7811d3b62cffc426947660e41be91d5
AbduAwais <abdu.awais@outlook.dk>
2026-03-14 23:19:49 +0100

    Implement routing with Home and NotFound pages; update App component structure

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+
 function App() {
   return (
-  <></>;
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+    </Routes>
+  </BrowserRouter>
   );  
 }
 
---
+++ Portfolio/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen " >
+     {/* Theme toggle */}
+
+     {/* Background */}
+
+      {/* Navbar */}
+
+      {/* Main content */}
+
+      {/* Footer */}
+    </div>
+  );
+}
